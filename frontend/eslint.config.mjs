c68c65e43733a1d87112f613e75031e1690538ef
Sitraka <heriniainasitraka222@gmail.com>
2026-02-27 15:20:31 +0100

    fix: remove ci package, restore restore lint in pipeline

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,9 @@
+// frontend/eslint.config.mjs
+export default {
+  root: true,
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    // règles custom si besoin
+    extends: "next",
+  },
+};
